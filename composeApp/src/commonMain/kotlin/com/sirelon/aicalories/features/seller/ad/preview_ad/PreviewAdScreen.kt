9ead91f366f37853cd8be97cbe3553f9b1fc573d
sirelon <sasha.sirelon@gmail.com>
2026-05-13 20:16:31 +0300

    Update files

--- composeApp/src/commonMain/kotlin/com/sirelon/aicalories/features/seller/ad/preview_ad/PreviewAdScreen.kt
+++ composeApp/src/commonMain/kotlin/com/sirelon/aicalories/features/seller/ad/preview_ad/PreviewAdScreen.kt
@@ -66,6 +66,9 @@ import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.scene.SinglePaneSceneStrategy
 import androidx.navigation3.ui.NavDisplay
+import androidx.navigationevent.NavigationEventInfo
+import androidx.navigationevent.compose.NavigationBackHandler
+import androidx.navigationevent.compose.rememberNavigationEventState
 import com.mohamedrejeb.calf.permissions.CoarseLocation
 import com.mohamedrejeb.calf.permissions.Permission
 import com.sirelon.sellsnap.designsystem.AppCard
@@ -258,6 +261,16 @@ fun PreviewAdScreen(
         entryDecorators = listOf(rememberSaveableStateHolderNavEntryDecorator<NavKey>()),
         entryProvider = entryProvider<NavKey> {
             entry<PreviewAdDestination.Content> {
+                val backHandlerState =
+                    rememberNavigationEventState(currentInfo = NavigationEventInfo.None)
+                NavigationBackHandler(
+                    state = backHandlerState,
+                    onBackCompleted = {
+                        if (navBackStack.lastOrNull() !is PreviewAdDestination.BackInfo) {
+                            navBackStack.add(PreviewAdDestination.BackInfo)
+                        }
+                    },
+                )
                 PreviewAdContentRoute(
                     viewModel = viewModel,
                     snackbarHostState = snackbarHostState,
