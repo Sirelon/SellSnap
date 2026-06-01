82fdb9a8837c3ba60af64da51f06c6d57cb3b3b4
sirelon <sasha.sirelon@gmail.com>
2026-06-01 18:18:14 +0300

    update deps.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.sirelon.sellsnap"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
